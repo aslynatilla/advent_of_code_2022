7da63defe1db20eaf22a58354baa0e7927544704
Antonio Natilla <antonio.natilla30@gmail.com>
2022-12-03 21:59:14 +0100

    End of 1st day, refactoring :star: :star:

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
-use std::fs::read_to_string;
-use std::vec;
-
-fn day_one(path_to_file: &str) -> u32 {
-    match read_to_string(path_to_file) {
-        Ok(calories_input) => {
-            let calories_list: Vec<Option<u32>> = calories_input
-                .lines()
-                .map(str::parse::<u32>)
-                .map(Result::ok)
-                .collect();
-
-            let mut calories_sums = vec![0u32];
-            for c in calories_list {
-                match c {
-                    Some(value) => {
-                        if let Some(last_value) = calories_sums.last_mut() {
-                            *last_value += value
-                        }
-                    }
-                    None => calories_sums.push(0u32),
-                }
-            }
-
-            match calories_sums.iter().max() {
-                Some(v) => *v,
-                None => panic!("Inappropriate input file"),
-            }
-        }
-        Err(_) => panic!("Could not open the file."),
-    }
-}
+mod day_one;
 
 fn main() {
-    let result = day_one("assets/input.txt");
-    println!("{}", result);
+    let (result_1, result_2) = day_one::solution("assets/input.txt");
+    println!("{}, {}", result_1, result_2);
 }

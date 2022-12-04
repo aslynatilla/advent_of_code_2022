4426118e9ca94be8ae4d9ef96ff8124e4f8c71fb
Antonio Natilla <antonio.natilla30@gmail.com>
2022-12-04 01:33:04 +0100

    Line to description conversion test

--- src/day_two/mod.rs
+++ src/day_two/mod.rs
@@ -1,8 +1,13 @@
+use std::fs::read_to_string;
+
+#[derive(Debug, PartialEq)]
 enum HandShape {
     Rock,
     Paper,
     Scissors,
 }
+
+#[derive(Debug, PartialEq)]
 struct RoundDescription {
     theirs: HandShape,
     ours: HandShape,
@@ -17,7 +22,7 @@ impl RoundDescription {
     }
 }
 
-fn line_to_round_description() -> RoundDescription {
+fn line_to_round_description(line: &str) -> RoundDescription {
     todo!()
 }
 
@@ -43,12 +48,23 @@ fn round_score(round: &RoundDescription) -> u32 {
     our_shape_score + our_outcome_score
 }
 
-fn solution() {
-    todo!()
+fn solution() -> u32 {
+    let input_data = match read_to_string("assets/input_day_two.txt") {
+        Ok(lines) => lines,
+        Err(e) => panic!("Input file not placed correctly\nReported as: {}", e),
+    };
+
+    input_data
+        .lines()
+        .map(line_to_round_description)
+        .map(|line| round_score(&line))
+        .sum()
 }
 
 #[cfg(test)]
 mod tests_day_two {
+    use crate::day_two::line_to_round_description;
+
     use super::{round_score, HandShape, RoundDescription};
 
     #[test]
@@ -60,5 +76,22 @@ mod tests_day_two {
         assert_eq!(round_score(&first_round), 8);
         assert_eq!(round_score(&second_round), 1);
         assert_eq!(round_score(&third_round), 6);
+    }
+
+    #[test]
+    fn conversion_test() {
+        let first_round = line_to_round_description("A Y");
+        let second_round = line_to_round_description("B X");
+        let third_round = line_to_round_description("C Z");
+        assert_eq!(first_round.theirs, HandShape::Rock);
+        assert_eq!(first_round.ours, HandShape::Paper);
+        assert_eq!(
+            second_round,
+            RoundDescription::new(HandShape::Paper, HandShape::Rock)
+        );
+        assert_eq!(
+            third_round,
+            RoundDescription::new(HandShape::Scissors, HandShape::Scissors)
+        );
     }
 }

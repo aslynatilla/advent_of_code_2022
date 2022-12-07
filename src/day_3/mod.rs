15d3583a2eb6b590a8e3c41cbf4b9357232eb3a9
Antonio Natilla <antonio.natilla30@gmail.com>
2022-12-07 22:42:03 +0100

    Priority computation for RucksackItem :100:

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -14,6 +14,21 @@ impl TryFrom<char> for RucksackItem {
     }
 }
 
+impl RucksackItem {
+    fn priority(&self) -> u64 {
+        let a_prio: u64 = 'a'.to_ascii_lowercase().into();
+        let uppercase_a_prio: u64 = 'A'.to_ascii_uppercase().into();
+
+        match self.0 {
+            ch @ 'a'..='z' => <char as Into<u64>>::into(ch.to_ascii_lowercase()) - a_prio + 1,
+            ch @ 'A'..='Z' => {
+                <char as Into<u64>>::into(ch.to_ascii_uppercase()) - uppercase_a_prio + 27
+            }
+            _ => panic!("Unexpected input"),
+        }
+    }
+}
+
 struct Rucksack {
     items: Vec<RucksackItem>,
 }
@@ -29,6 +44,15 @@ impl From<&str> for Rucksack {
     }
 }
 
+impl Rucksack {
+    fn compartments(&self) -> (&[RucksackItem], &[RucksackItem]) {
+        let sack_length = self.items.len();
+        assert!(sack_length.rem_euclid(2) == 0);
+        let half_size = sack_length / 2;
+        (&self.items[0..half_size], &self.items[half_size..])
+    }
+}
+
 struct RucksackList {
     rucksacks: Vec<Rucksack>,
 }
@@ -38,6 +62,16 @@ impl<'a> From<Lines<'a>> for RucksackList {
         RucksackList {
             rucksacks: { lines.map(Rucksack::from).collect() },
         }
+    }
+}
+
+impl IntoIterator for RucksackList {
+    type Item = Rucksack;
+
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.rucksacks.into_iter()
     }
 }
 
@@ -52,4 +86,24 @@ pub fn solution() -> (u64, u64) {
     .into();
 
     (result, 0u64)
+}
+
+mod tests {
+
+    #[test]
+    fn priorities() {
+        let characters: Vec<_> = vec!['p', 'L', 'P', 'v', 't', 's']
+            .into_iter()
+            .map(super::RucksackItem::try_from)
+            .map(Result::unwrap)
+            .collect();
+        let expected_priorities = vec![16u64, 38, 42, 22, 20, 19];
+        assert!(characters
+            .iter()
+            .zip(expected_priorities.iter())
+            .all(|(char, &prio)| {
+                println!("{}", char.priority());
+                char.priority() == prio
+            }))
+    }
 }
